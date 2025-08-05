74cb2c221a1b7b006cdd1323ed7acf327912b9f1
Giga <gianlucacabrera@gmail.com>
2025-08-05 15:28:52 -0300

    add project list draft

--- react-app/src/components/Projects.tsx
+++ react-app/src/components/Projects.tsx
@@ -1,34 +1,35 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Projects = () => {
+  const [index, setIndex] = useState(0);
+
+  interface ListItemProps {
+    projectName: string;
+    itemIndex: number;
+  }
+
+  function ListItem ({ projectName, itemIndex }: ListItemProps) {
+    return (
+      <>
+        <div>{projectName}</div>
+        <button>{index == itemIndex ? '-' : '+'}</button>
+      </>
+    )
+  }
+
   return (
     <section id="projects" className="projects">
       <hr className="section-delimiter" />
       <h1 className="section-header">Projects</h1>
       <hr className="section-delimiter" />
       <div className="project-grid" >
-        <div className="project-container">
-          <div className="project-image-container">
-            <img className="project-image" src="assets/placeholder.jpg" />
-          </div>
-          <div className="project-description-container">
-            <p>
-              A short description of the project
-            </p>
-            <button>View on GitHub</button>
-          </div>
+        <div>
+          <ListItem projectName="item 1" itemIndex ={1}></ListItem>
+          <ListItem projectName="item 2" itemIndex ={2}></ListItem>
+          <ListItem projectName="item 3" itemIndex ={3}></ListItem>
         </div>
-        <div className="project-container">
-          <div className="project-image-container">
-            <img className="project-image" src="assets/placeholder.jpg" />
-          </div>
-          <div className="project-description-container">
-            <p>
-              A short description of the project
-            </p>
-            <button>View on GitHub</button>
-          </div>
-        </div>
+        <div>2</div>
       </div>
     </section>
   )
