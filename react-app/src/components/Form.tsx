d4e551b4027749aa1f88956f7cd6a815df2fa57e
Giga <gianlucacabrera@gmail.com>
2025-08-08 13:49:00 -0300

    add submitting the form and email.js connection

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,19 +1,43 @@
-import { useState } from 'react'; 
+import { useState } from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
   const [form, setForm] = useState({name: '', email: '', message: ''})
+  const gmailForm = useRef<HTMLFormElement>(null);
+  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target;
     setForm(prev => ({...prev, [name]: value}))
   }
+
+  function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_fzwkn44',
+        'template_49qekbg',
+        gmailForm.current!,
+        {
+          publicKey: publicKey,
+        })
+      .then(
+        () => {
+          console.log('Message sent!');
+        },
+        (error) => {
+          console.log('Failed to send message:', error.text);
+        },
+      );
+  }
   return (
-    <form>
-      <input type="text" name="name" value={form.name} placeholder='Your Name' onChange={handleChange}></input>
-      <input type="text" name="email" value={form.email} placeholder='Your Email Address' onChange={handleChange}></input>
-      <input type="text" name="message" value={form.message} placeholder="Your Message" onChange={handleChange}></input>
-      <input type="submit"></input>
-      
+    <form ref={gmailForm} onSubmit={handleSubmit}>
+      <input type="text" name="name" value={form.name} placeholder='Your Name' onChange={handleChange} required></input>
+      <input type="text" name="email" value={form.email} placeholder='Your Email Address' onChange={handleChange} required></input>
+      <input type="text" name="message" value={form.message} placeholder="Your Message" onChange={handleChange} required></input>
+      <button type="submit">Submit</button>
     </form>
   )
 }
