25da5a802a4053b76a5d6411cb6278bfe449ec5a
Giga <gianlucacabrera@gmail.com>
2025-08-09 12:25:31 -0300

    add form submission confirmation message

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -6,6 +6,9 @@ const Form = () => {
   const [form, setForm] = useState({name: '', email: '', message: ''})
   const gmailForm = useRef<HTMLFormElement>(null);
   const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+  const confirmationMessage = document.getElementById('confirmation-message') as HTMLElement;
+  const [messageSent, setMessageSent] = useState<boolean | null>(null);
+
 
   function handleChange (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = e.target;
@@ -26,9 +29,15 @@ const Form = () => {
       .then(
         () => {
           console.log('Message sent!');
+          setMessageSent(true);
+          confirmationMessage.style.display = 'block';
+          setTimeout(() => confirmationMessage.style.display = 'none', 3000);
         },
         (error) => {
           console.log('Failed to send message:', error.text);
+          setMessageSent(false);
+          confirmationMessage.style.display = 'block';
+          setTimeout(() => confirmationMessage.style.display = 'none', 3000);
         },
       );
   }
@@ -38,6 +47,7 @@ const Form = () => {
       <input type="text" className="input-field" name="email" value={form.email} placeholder='Your Email Address' onChange={handleChange} required></input>
       <textarea className="input-field" maxLength={200} name="message" rows={6} value={form.message} placeholder="Your Message" onChange={handleChange} required></textarea>
       <button type="submit" className="submit-button">Submit</button>
+      <div id="confirmation-message" className="confirmation-message">{messageSent ? 'Message sent!' : 'Failed to send message.'}</div>
     </form>
   )
 }
