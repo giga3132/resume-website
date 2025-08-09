37ecb6a57e6d4e445b5d83c5d7f5a413cbe29f41
Giga <gianlucacabrera@gmail.com>
2025-08-09 11:42:15 -0300

    update media queries

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -7,7 +7,7 @@ const Form = () => {
   const gmailForm = useRef<HTMLFormElement>(null);
   const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-  function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const { name, value } = e.target;
     setForm(prev => ({...prev, [name]: value}))
   }
@@ -36,7 +36,7 @@ const Form = () => {
     <form ref={gmailForm} className="form" onSubmit={handleSubmit}>
       <input type="text" className="input-field" name="name" value={form.name} placeholder='Your Name' onChange={handleChange} required></input>
       <input type="text" className="input-field" name="email" value={form.email} placeholder='Your Email Address' onChange={handleChange} required></input>
-      <input type="text" className="input-field" name="message" value={form.message} placeholder="Your Message" onChange={handleChange} required></input>
+      <textarea className="input-field" maxLength={200} name="message" rows={6} value={form.message} placeholder="Your Message" onChange={handleChange} required></textarea>
       <button type="submit" className="submit-button">Submit</button>
     </form>
   )
